445b82c76fc8d0395c8d05003e34184fd9d2c4f1
Bilal </bilal@nathanhr.com>
2024-05-20 15:17:18 +0400

    Middleware integration

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const logger = require('./logger')
+
+const axios = require('axios');
+
 const { products } = require('./data');
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('/', logger,(req, res) => {
     console.log('Sending home page');
     res.status(200).sendFile(path.resolve(__dirname, 'index.html'));
 });
@@ -37,20 +41,42 @@ app.get('/api/products/:productID', (req, res) => {
 app.get('/api/v1/query', (req, res) => {
     let sortedProduct = [...products]
 
-    let {search, limit} = req.query
+    let { search, limit } = req.query
 
-    if(search) {
+    if (search) {
         sortedProduct = sortedProduct.filter((product) => {
             return product.name.startsWith(search)
         })
     }
 
-    if(limit) {
+    if (limit) {
         sortedProduct = sortedProduct.slice(0, Number(limit))
     }
 
     res.status(200).send(sortedProduct)
 })
+
+// async function callZenQuotesAPI100Times() {
+//     const url = 'https://zenquotes.io/api/random';
+//     const responses = [];
+
+//     for (let i = 0; i < 100000; i++) {
+//         try {
+//             const response = await axios.get(url);
+//             const data = response.data;
+//             responses.push(data);
+//             console.log(`Call ${i + 1}: Success - ${data[0].q} - ${data[0].a}`);
+//         } catch (error) {
+//             console.error(`Call ${i + 1}: Error occurred - ${error}`);
+//         }
+//     }
+
+//     return responses;
+// }
+
+// // Call the function
+// callZenQuotesAPI100Times();
+
 
 // Catch-all route for handling 404 errors
 app.get('*', (req, res) => {
